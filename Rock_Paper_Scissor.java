f996877ce672ef854bc06b1ecfa26a4077ca3c9f
Md. Abu Talha <101306544+md-abutalha@users.noreply.github.com>
2022-11-20 18:09:16 +0600

    Update Rock_Paper_Scissor.java

--- Rock_Paper_Scissor.java
+++ Rock_Paper_Scissor.java
@@ -1,27 +1,37 @@
 package abu_talha;
 
 import java.util.Random;
-import java.util.Scanner;
+import  java.util.Scanner;
 public class at_41practise {
     public static void main(String[] args) {
         //0 for rock
         //1 for paper
-        //2 for seaser
+        //2 for scissor
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 0 for rock: 1 for paper:  2 for Scissor:");
-        int userInput = sc.nextInt(3);
+        System.out.println("Enter number 0 for Rock: 1 for paper: 2 for scissor: ");
+         int userInput = sc.nextInt();
 
-        Random random = new Random();
-        int computerInput = random.nextInt(3);
+         Random random = new Random();
+         int computerInput = random.nextInt(3);
 
-        if (userInput == computerInput){
-            System.out.println("Draw");
-        } else if (userInput == 0 && computerInput == 2 || userInput == 1 && computerInput == 0 || userInput == 2 && computerInput == 1) {
-            System.out.println("you win!");
-        }else {
-            System.out.println("computer win");
-        }
-        System.out.println("comuter choice "+ computerInput);
+         if (userInput == computerInput){
+             System.out.println("Draw");
+         }else if (userInput == 0 && computerInput == 2 || userInput == 1 && computerInput == 0 || userInput == 2 && computerInput == 1 ){
+             System.out.println("you win!");
+         }
+         else {
+             System.out.println("computer win!");
+         }
+        //System.out.println("Computer choice: "+ computerInput);
+
+         if (computerInput == 0){
+             System.out.println("computer chose : Rock");
+         } else if (computerInput == 1) {
+             System.out.println("computer chose : paper");
+         }else if (computerInput == 2){
+             System.out.println("computer chose: scissor");
+         }
+
 
     }
 }
